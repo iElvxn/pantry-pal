192ddb442923521e7f759b521215875ce69b7fb0
Elvin <elvinly323@gmail.com>
2024-07-30 23:22:08 -0400

    Set up Firebase

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -8,14 +8,14 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
+    apiKey: "AIzaSyDvXH4kHAcYpA4xdDCeylWAYx8GavaA4Fo",
     authDomain: "pantry-pal-1aac3.firebaseapp.com",
     projectId: "pantry-pal-1aac3",
     storageBucket: "pantry-pal-1aac3.appspot.com",
     messagingSenderId: "859761238994",
     appId: "1:859761238994:web:3086038ad889225654de0b",
     measurementId: "G-NXE4R3MSZK"
-};
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -23,3 +23,5 @@ const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
+
+
